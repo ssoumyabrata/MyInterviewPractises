8912465f9f43035819119c423edafaa878cfe120
Soumyabrata Saha <soumyabrata.ceiindia@gmail.com>
2026-06-27 23:44:13 +0530

    anagram fix

--- practise/src/main/java/com/practise/Anagram.java
+++ practise/src/main/java/com/practise/Anagram.java
@@ -13,11 +13,11 @@ public class Anagram {
             return false;
 
         for (char ch : str1.toCharArray()) {
-            map1.put(ch, map1.getOrDefault(map1, 0) + 1);
+            map1.put(ch, map1.getOrDefault(ch, 0) + 1);
         }
 
         for (char ch : str2.toCharArray()) {
-            map2.put(ch, map2.getOrDefault(map2, 0) + 1);
+            map2.put(ch, map2.getOrDefault(ch, 0) + 1);
         }
 
         return map1.equals(map2);
@@ -30,7 +30,7 @@ public class Anagram {
             return false;
 
         for (char ch : str1.toCharArray()) {
-            map1.put(ch, map1.getOrDefault(map1, 0) + 1);
+            map1.put(ch, map1.getOrDefault(ch, 0) + 1);
         }
 
         for (char ch : str2.toCharArray()) {
